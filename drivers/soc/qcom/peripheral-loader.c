bff819cdf37eb863e0eb9cc053c7cf062d83d589
Asderdd <aliihsan.hatun@turkdevs.com>
2017-11-24 16:59:04 +0000

    soc: qcom: Remove vendor related codes

--- drivers/soc/qcom/peripheral-loader.c
+++ drivers/soc/qcom/peripheral-loader.c
@@ -42,9 +42,6 @@
 
 #include "peripheral-loader.h"
 
-#include <soc/qcom/vendor/sdlog_mem_reserve.h>
-#include <soc/qcom/vendor/vlog.h>
-
 
 #define pil_err(desc, fmt, ...)						\
 	dev_err(desc->dev, "%s: " fmt, desc->name, ##__VA_ARGS__)
@@ -228,51 +225,11 @@ int pil_assign_mem_to_subsys_and_linux(struct pil_desc *desc,
 	ret = hyp_assign_phys(addr, size, srcVM, 1, destVM, destVMperm, 2);
 	if (ret)
 		pil_err(desc, "%s: failed for %pa address of size %zx - subsys VMid %d\n",
-			__func__, &addr, size, desc->subsys_vmid);
-
-	if ((desc->subsys_vmid == VMID_MSS_MSA) && sdlog_memory_reserved()) {
-		ret = hyp_assign_phys(sdlog_memory_get_addr(),
-			sdlog_memory_get_size(), srcVM, 1,
-			destVM, destVMperm, 2);
-		pil_err(desc, "%s: assign sdlog memory for %pa address of size %zx\n",
-			__func__, &addr, size);
-	}
-
-
-#ifdef CONFIG_VENDOR_VLOG
-	if ((desc->subsys_vmid == VMID_MSS_MSA) &&
-			vendor_log_get_memory_addr() &&
-			vendor_log_get_memory_size()) {
-		ret = hyp_assign_phys(vendor_log_get_memory_addr(),
-			vendor_log_get_memory_size(), srcVM, 1,
-			destVM, destVMperm, 2);
-		pil_info(desc, "%s: assign vlog memory addr 0x%x, size 0x%x\n",
-			__func__, vendor_log_get_memory_addr(),
-			vendor_log_get_memory_size());
-	}
-#endif
+				__func__, &addr, size, desc->subsys_vmid);
 
 	return ret;
 }
 EXPORT_SYMBOL(pil_assign_mem_to_subsys_and_linux);
-
-int pil_assign_sdlog_mem_back_to_linux(struct pil_desc *desc)
-{
-	int ret = 0;
-	int srcVM[2] = {VMID_HLOS, VMID_MSS_MSA};
-	int destVM[1] = {VMID_HLOS};
-	int destVMperm[1] = {PERM_READ | PERM_WRITE};
-
-	if (sdlog_memory_reserved()) {
-		ret = hyp_assign_phys(sdlog_memory_get_addr(),
-			sdlog_memory_get_size(),
-			srcVM, 2, destVM, destVMperm, 1);
-		pil_info(desc, "assign sdlog memoy back to linux\n");
-	}
-
-	return ret;
-}
-
 
 int pil_reclaim_mem(struct pil_desc *desc, phys_addr_t addr, size_t size,
 						int VMid)
